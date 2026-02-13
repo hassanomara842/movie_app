2402613b93041cb9037bb63bc7b760085e30b40b
Hassan <hassanomara842@gmail.com>
2026-02-14 00:25:30 +0200

    feat: add app colors class

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class AppColors {
+  static const Color primaryBlack = Color(0xFF121312);
+  static const Color darkGrey = Color(0xFF282A28);
+  static const Color primaryYellow = Color(0xFFFFBB3B);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color successGreen = Color(0xFF57AA53);
+  static const Color errorRed = Color(0xFFE82626);
+}
