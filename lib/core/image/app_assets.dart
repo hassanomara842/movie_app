f01be8489a64b0fe7d51fca2915ce2c5cdc9097b
Nour Muhammed Mahmoud <145355514+nouramer725@users.noreply.github.com>
2026-02-14 03:10:35 +0200

    add easy localizations packages. edit images in assets file. edit image in app_assets.

--- lib/core/image/app_assets.dart
+++ lib/core/image/app_assets.dart
@@ -1,4 +1,4 @@
 class AppImages {
-  static const String onboardingImage1 = 'assets/images/onboarding_image1.png';
+  static const String onboardingImage1 = 'assets/images/onboadring.png';
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/provider/app_language_provider.dart';
 import 'package:movie_app/provider/app_theme_provider.dart';
@@ -11,16 +12,22 @@ import 'package:provider/provider.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await EasyLocalization.ensureInitialized();
   await SharedPreferencesLanguage.init();
   await SharedPreferencesTheme.init();
 
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AppLanguageProvider()),
-        ChangeNotifierProvider(create: (_) => AppThemeProvider()),
-      ],
-      child: const MyApp(),
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('en'),
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AppLanguageProvider()),
+          ChangeNotifierProvider(create: (_) => AppThemeProvider()),
+        ],
+        child: const MyApp(),
+      ),
     ),
   );
 }
@@ -30,15 +37,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var languageProvider = Provider.of<AppLanguageProvider>(context);
     var themeProvider = Provider.of<AppThemeProvider>(context);
-    
+
     return MaterialApp(
       title: 'Movie App',
-      locale: Locale(languageProvider.appLocal),
+      ///local
+      locale: context.locale,
+      supportedLocales: context.supportedLocales,
+      localizationsDelegates: context.localizationDelegates,
+
+      ///theme
       themeMode: themeProvider.appTheme,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+
       debugShowCheckedModeBanner: false,
       initialRoute: AppRoutes.home,
       onGenerateRoute: AppRoutes.onGenerateRoute,
