0aa9e38d1eec1e5e153391fd96db6d60c0f698e1
Dell 5540 <ahmednosaeer@gmail.com>
2026-02-16 00:10:52 +0200

    feat: update profile fields and add buttons at bottom
    - Modified username and phone number containers
    - Added text labels in Arabic and English
    - Added Delete and Update buttons at the bottom of the screen

--- lib/profile_screen/update_profile_screen.dart
+++ lib/profile_screen/update_profile_screen.dart
@@ -5,7 +5,7 @@ import 'package:movie_app/core/image/app_assets.dart';
 import 'package:movie_app/core/responsive/responsive.dart';
 import 'package:movie_app/core/text/app_text.dart';
 class UpdateProfileScreen extends StatelessWidget {
-   UpdateProfileScreen({super.key});
+   const UpdateProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -18,51 +18,111 @@ class UpdateProfileScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 40,),
-            CircleAvatar(
-              radius: 80,
-              backgroundImage: AssetImage(AppImages.avatar),
-            ),
-            SizedBox(height: 20,),
-
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              padding: EdgeInsets.symmetric(horizontal: 15,vertical:7),
-              decoration: BoxDecoration(
-                color: AppColors.darkGrey,
-                borderRadius: BorderRadius.circular(15),
+        child: Padding(
+          padding:  const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            children: [
+              const SizedBox(height: 40,),
+              const CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage(AppImages.avatar),
               ),
-              child: TextField(
-                style: ( AppText.regularText(color: AppColors.white, fontSize:20 )),
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  icon: Icon(Icons.person, color: AppColors.white),
-                  hintText: "John Safwat",
-                  hintStyle: TextStyle(color: AppColors.white),
+              const SizedBox(height: 30,),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15,vertical:4),
+                decoration: BoxDecoration(
+                  color: AppColors.darkGrey,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextField(
+                  style: ( AppText.regularText(color: AppColors.white, fontSize:20 )),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: Icon(Icons.person, color: AppColors.white),
+                    hintText: "John Safwat",
+                    hintStyle: TextStyle(color: AppColors.white),
+                  ),
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              padding: EdgeInsets.symmetric(horizontal: 15,vertical:7),
-              decoration: BoxDecoration(
-                color: AppColors.darkGrey,
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: TextField(
-                style: ( AppText.regularText(color: AppColors.white, fontSize:20 )),
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  icon: Icon(Icons.phone_rounded, color: AppColors.white),
-                  hintText: "0114333333",
-                  hintStyle: TextStyle(color: AppColors.white),
+              const SizedBox(height: 20,),
+
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15,vertical:4),
+                decoration: BoxDecoration(
+                  color: AppColors.darkGrey,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextField(
+                  style: ( AppText.regularText(color: AppColors.white, fontSize:20 )),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: Icon(Icons.phone_rounded, color: AppColors.white),
+                    hintText: "0114333333",
+                    hintStyle: TextStyle(color: AppColors.white),
+                  ),
                 ),
               ),
-            ),
-
-          ],
+              const SizedBox(height: 10),
+              Align(alignment: AlignmentGeometry.topStart,
+                child: TextButton(
+                  onPressed: () {
+                    print("Reset Password clicked");
+                  },
+                  child: Text(
+                    "Reset Password".tr(),
+                    textAlign: TextAlign.start,
+                    style:
+                      AppText.regularText(color: AppColors.white, fontSize: 20)
+                  ),
+                ),
+              ),
+              Spacer(),
+              Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity, // ياخد عرض الشاشة بالكامل
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("Delete clicked");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:AppColors.errorRed,
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      child: Text(
+                        "Delete Account".tr(),
+                        style: AppText.regularText(color: AppColors.white, fontSize: 20)
+                        ),
+                      ),
+                    ),
+                  const SizedBox(height: 16),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        print("");
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor:AppColors.primaryYellow,
+                        padding: EdgeInsets.symmetric(vertical: 14),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      child: Text(
+                          "Update Data".tr(),
+                          style: AppText.regularText(color: AppColors.primaryBlack, fontSize: 20)
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 35),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
