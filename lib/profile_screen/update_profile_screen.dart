08ace6d2cec69beba4129be241f1f98994ad6b8c
Dell 5540 <ahmednosaeer@gmail.com>
2026-02-17 01:39:00 +0200

    Update profile screen UI"

--- lib/profile_screen/update_profile_screen.dart
+++ lib/profile_screen/update_profile_screen.dart
@@ -24,7 +24,7 @@ class UpdateProfileScreen extends StatelessWidget {
           padding:  EdgeInsets.symmetric(horizontal: w(20)),
           child: Column(
             children: [
-               SizedBox(height: h(35),),
+               SizedBox(height: h(34),),
                CircleAvatar(
                 radius: w(80),
                 backgroundImage: const AssetImage(AppImages.avatar),
@@ -59,7 +59,7 @@ class UpdateProfileScreen extends StatelessWidget {
                   decoration: const InputDecoration(
                     border: InputBorder.none,
                     icon: Icon(Icons.phone_rounded, color: AppColors.white),
-                    hintText: "0111233444",
+                    hintText: "015666665544",
                     hintStyle: TextStyle(color: AppColors.white),
                   ),
                 ),
